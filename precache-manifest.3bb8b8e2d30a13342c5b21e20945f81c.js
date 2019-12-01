862ea50bb50f4e774578b6c47a716530a00b4d57
emmafattori <emmalfattori@gmail.com>
2019-12-01 13:05:58 -0500

    Updates

---
+++ precache-manifest.3bb8b8e2d30a13342c5b21e20945f81c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e69aca1705e5bb77569be5bcb4be9d3a",
+    "url": "/project-5/index.html"
+  },
+  {
+    "revision": "15b7bf18f067dccdf23a",
+    "url": "/project-5/static/css/main.6ed1bef0.chunk.css"
+  },
+  {
+    "revision": "cd6af69e9af27b530c6c",
+    "url": "/project-5/static/js/2.cf4793d1.chunk.js"
+  },
+  {
+    "revision": "15b7bf18f067dccdf23a",
+    "url": "/project-5/static/js/main.bc490cc0.chunk.js"
+  },
+  {
+    "revision": "b475942e76c1dcd295df",
+    "url": "/project-5/static/js/runtime-main.ece5c7e8.js"
+  },
+  {
+    "revision": "24b9824835fca5b13953cb7216c39eeb",
+    "url": "/project-5/static/media/sunflower2soft.24b98248.png"
+  }
+]);
